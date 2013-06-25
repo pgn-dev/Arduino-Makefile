1f3d56afe898c51f14a99873f11be426453fca6b
meanbot <10bec066@nirmauni.ac.in>
2013-06-25 22:33:27 +0530

    Add HelloWorldUserspace.ino
    Added HelloWorldUserspace.ino and corresponding makefile as examples
    for latest changes.

---
+++ examples/HelloWorldUserspace/HelloWorldUserspace.ino
@@ -0,0 +1,15 @@
+/* 
+ * HelloWorldUserspace
+ * A program to print helloworld in the Beaglebone
+ *
+ *
+ */
+#include "Arduino.h"
+
+void setup() {
+	printf("hello, world!\n");
+}
+
+void loop() {
+
+}
